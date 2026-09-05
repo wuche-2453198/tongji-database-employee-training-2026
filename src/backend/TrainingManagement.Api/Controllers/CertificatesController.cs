8d62970ab42e5f2d70994fcdb32aff7d098b58f0
tongsir-9527 <323358968@qq.com>
2026-09-05 09:39:53 +0800

    Retrieve employee ID from claims in GetMyCertificates

--- src/backend/TrainingManagement.Api/Controllers/CertificatesController.cs
+++ src/backend/TrainingManagement.Api/Controllers/CertificatesController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using TrainingManagement.Api.Common;
 using TrainingManagement.Api.Dtos.Certificates;
@@ -27,7 +28,13 @@ namespace TrainingManagement.Api.Controllers
         [HttpGet("my")]
         public async Task<ActionResult<ApiResponse<object>>> GetMyCertificates()
         {
-            var employeeId = 1;
+            var employeeIdClaim = User.FindFirst("emp_id") ?? User.FindFirst(ClaimTypes.NameIdentifier);
+            if (employeeIdClaim == null)
+            {
+                return Unauthorized(ApiResponse<bool>.Fail("无法获取用户ID"));
+            }
+
+            var employeeId = int.Parse(employeeIdClaim.Value);
             var result = await _certificateService.GetMyCertificatesAsync(employeeId);
             return Ok(ApiResponse<object>.Success(result, "查询成功"));
         }
