b1eb5c0345b5f37932fcc48256f36baf23cf94c3
tongsir-9527 <323358968@qq.com>
2026-09-07 19:50:19 +0800

    Update IRatingRepository.cs

--- src/backend/TrainingManagement.Api/Repositories/Interfaces/IRatingRepository.cs
+++ src/backend/TrainingManagement.Api/Repositories/Interfaces/IRatingRepository.cs
@@ -1,14 +1,14 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TrainingManagement.Api.Dtos.Ratings;
+using TrainingManagement.Api.Entities;
 
-namespace TrainingManagement.Api.Repositories.Interfaces
+namespace TrainingManagement.Api.Repositories.Interfaces;
+public interface IRatingRepository
 {
-    public interface IRatingRepository
-    {
-        Task<bool> CreateAsync(CreateRatingRequest request, int employeeId);
-        Task<object> GetListAsync(int? courseId, int? trainerId, int page, int pageSize);
-        Task<bool> ExistsByEmployeeAndCourseAsync(int employeeId, int courseId);
-        Task<object> GetByIdAsync(int ratingId);
-        Task<bool> VerifyAsync(int ratingId, string verifyComment);
-    }
+    Task<bool> CreateAsync(CreateRatingRequest request, int employeeId);
+    Task<IEnumerable<TrainerRating>> GetListAsync(int? courseId, int? trainerId, int page, int pageSize);
+    Task<bool> ExistsByEmployeeAndCourseAsync(int employeeId, int courseId);
+    Task<TrainerRating?> GetByIdAsync(int ratingId);
+    Task<bool> VerifyAsync(int ratingId, string verifyComment, int hrVerifierEmpId);
 }
