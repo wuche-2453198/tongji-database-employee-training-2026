2f06d9ae826699a5c9dfe5815c32dd402839a30e
tongsir-9527 <323358968@qq.com>
2026-09-07 19:58:37 +0800

    Update ICertificateService.cs

--- src/backend/TrainingManagement.Api/Services/Interfaces/ICertificateService.cs
+++ src/backend/TrainingManagement.Api/Services/Interfaces/ICertificateService.cs
@@ -1,13 +1,13 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TrainingManagement.Api.Dtos.Certificates;
+using TrainingManagement.Api.Entities;
 
-namespace TrainingManagement.Api.Services.Interfaces
+namespace TrainingManagement.Api.Services.Interfaces;
+public interface ICertificateService
 {
-    public interface ICertificateService
-    {
-        Task<object> GenerateCertificateAsync(GenerateCertificateRequest request);
-        Task<object> GetMyCertificatesAsync(int employeeId);
-        Task<object> GetCertificateByIdAsync(int id);
-        Task<bool> MarkNotifiedAsync(int id);
-    }
+    Task<CertificateResult> GenerateCertificateAsync(GenerateCertificateRequest request, int issuedByEmpId);
+    Task<IEnumerable<TrainingCertificate>> GetMyCertificatesAsync(int employeeId);
+    Task<TrainingCertificate> GetCertificateByIdAsync(int id);
+    Task<bool> MarkNotifiedAsync(int id);
 }
