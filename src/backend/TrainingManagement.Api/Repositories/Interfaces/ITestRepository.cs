6ecabff425a37a277664d90c7aad198f9fa45556
tongsir-9527 <323358968@qq.com>
2026-09-07 19:50:41 +0800

    Update ITestRepository.cs

--- src/backend/TrainingManagement.Api/Repositories/Interfaces/ITestRepository.cs
+++ src/backend/TrainingManagement.Api/Repositories/Interfaces/ITestRepository.cs
@@ -1,13 +1,13 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TrainingManagement.Api.Dtos.Tests;
+using TrainingManagement.Api.Entities;
 
-namespace TrainingManagement.Api.Repositories.Interfaces
+namespace TrainingManagement.Api.Repositories.Interfaces;
+public interface ITestRepository
 {
-    public interface ITestRepository
-    {
-        Task<bool> CreateAsync(CreateTestRequest request);
-        Task<bool> UpdateScoreAsync(CreateTestRequest request);
-        Task<bool> ExistsByEmployeeCourseAndTypeAsync(int employeeId, int courseId, string testType);
-        Task<object> GetListAsync(int? employeeId, int? courseId, string? testType);
-    }
+    Task<bool> CreateAsync(CreateTestRequest request, int recordedByEmpId);
+    Task<bool> UpdateScoreAsync(CreateTestRequest request, int recordedByEmpId);
+    Task<bool> ExistsByEmployeeCourseAndTypeAsync(int employeeId, int courseId, string testType);
+    Task<IEnumerable<TrainingTest>> GetListAsync(int? employeeId, int? courseId, string? testType);
 }
