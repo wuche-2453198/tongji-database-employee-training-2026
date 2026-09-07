a9ce7290d82023735f24e1ef9da1a7860285a01e
tongsir-9527 <323358968@qq.com>
2026-09-07 22:12:45 +0800

    Update CreateRatingRequest.cs

--- src/backend/TrainingManagement.Api/Dtos/Ratings/CreateRatingRequest.cs
+++ src/backend/TrainingManagement.Api/Dtos/Ratings/CreateRatingRequest.cs
@@ -3,6 +3,6 @@ public class CreateRatingRequest
 {
     public int CourseId { get; set; }
     public int TrainerId { get; set; }
-    public int Score { get; set; }    // 1-5
-    public string? Comment { get; set; }
+    public int Score { get; set; }
+    public string? RatingComment { get; set; }
 }
