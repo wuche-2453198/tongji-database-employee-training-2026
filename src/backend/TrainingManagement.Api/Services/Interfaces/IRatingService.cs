34f92cefd526155d5c79fab4991e10e104a666bb
tongsir-9527 <323358968@qq.com>
2026-09-07 19:58:01 +0800

    Refactor IRatingService methods and parameters
    Updated the IRatingService interface to return IEnumerable<TrainerRating> instead of object for GetRatingListAsync and added hrVerifierEmpId parameter to VerifyRatingAsync.

--- src/backend/TrainingManagement.Api/Services/Interfaces/IRatingService.cs
+++ src/backend/TrainingManagement.Api/Services/Interfaces/IRatingService.cs
@@ -1,12 +1,12 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TrainingManagement.Api.Dtos.Ratings;
+using TrainingManagement.Api.Entities;
 
-namespace TrainingManagement.Api.Services.Interfaces
+namespace TrainingManagement.Api.Services.Interfaces;
+public interface IRatingService
 {
-    public interface IRatingService
-    {
-        Task<bool> CreateRatingAsync(CreateRatingRequest request, int employeeId);
-        Task<object> GetRatingListAsync(int? courseId, int? trainerId, int page, int pageSize);
-        Task<bool> VerifyRatingAsync(int ratingId, string verifyComment);
-    }
+    Task<bool> CreateRatingAsync(CreateRatingRequest request, int employeeId);
+    Task<IEnumerable<TrainerRating>> GetRatingListAsync(int? courseId, int? trainerId, int page, int pageSize);
+    Task<bool> VerifyRatingAsync(int ratingId, string verifyComment, int hrVerifierEmpId);
 }
