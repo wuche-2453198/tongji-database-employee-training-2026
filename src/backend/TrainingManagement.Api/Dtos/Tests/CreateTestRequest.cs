c81f2545fc13f81610aff5bfa3e58977aa8d5e43
tongsir-9527 <323358968@qq.com>
2026-09-07 22:13:13 +0800

    Update CreateTestRequest.cs

--- src/backend/TrainingManagement.Api/Dtos/Tests/CreateTestRequest.cs
+++ src/backend/TrainingManagement.Api/Dtos/Tests/CreateTestRequest.cs
@@ -1,7 +1,7 @@
 namespace TrainingManagement.Api.Dtos.Tests;
 public class CreateTestRequest
 {
-    public int EmployeeId { get; set; }
+    public int EmpId { get; set; }
     public int CourseId { get; set; }
     public string TestType { get; set; } = "";
     public int Score { get; set; }
