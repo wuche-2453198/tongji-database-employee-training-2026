9ea3e7068952c55d6849e14d869c441157e5d982
tongsir-9527 <323358968@qq.com>
2026-09-07 19:57:07 +0800

    Create CertificateResult.cs

---
+++ src/backend/TrainingManagement.Api/Dtos/Certificates/CertificateResult.cs
@@ -0,0 +1,7 @@
+namespace TrainingManagement.Api.Dtos.Certificates;
+
+public class CertificateResult
+{
+    public int CertificateId { get; set; }
+    public string CertificateCode { get; set; } = "";
+}
