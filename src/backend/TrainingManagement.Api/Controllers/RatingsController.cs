a4b8040d451acbc2adc4671aa68e2d40fb63e9a5
tongsir-9527 <323358968@qq.com>
2026-09-07 15:41:28 +0800

    Add using statement for Responses in RatingsController

--- src/backend/TrainingManagement.Api/Controllers/RatingsController.cs
+++ src/backend/TrainingManagement.Api/Controllers/RatingsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using TrainingManagement.Api.Common;
+using TrainingManagement.Api.Common.Responses;
 using TrainingManagement.Api.Dtos.Ratings;
 using TrainingManagement.Api.Services.Interfaces;
 
