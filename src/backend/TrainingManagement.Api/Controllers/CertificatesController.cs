3eb923a920b7ec4f85e37fadc0a036f7a3425238
tongsir-9527 <323358968@qq.com>
2026-09-07 09:25:57 +0800

    Update using directive for Responses namespace

--- src/backend/TrainingManagement.Api/Controllers/CertificatesController.cs
+++ src/backend/TrainingManagement.Api/Controllers/CertificatesController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using TrainingManagement.Api.Common;
+using TrainingManagement.Api.Common.Responses;
 using TrainingManagement.Api.Dtos.Certificates;
 using TrainingManagement.Api.Services.Interfaces;
 
