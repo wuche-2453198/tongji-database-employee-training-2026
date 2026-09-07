9bfee69d1779579bdb4eae47e2a5a6af85554bb4
tongsir-9527 <323358968@qq.com>
2026-09-07 19:58:22 +0800

    Update ITestService.cs

--- src/backend/TrainingManagement.Api/Services/Interfaces/ITestService.cs
+++ src/backend/TrainingManagement.Api/Services/Interfaces/ITestService.cs
@@ -1,11 +1,11 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TrainingManagement.Api.Dtos.Tests;
+using TrainingManagement.Api.Entities;
 
-namespace TrainingManagement.Api.Services.Interfaces
+namespace TrainingManagement.Api.Services.Interfaces;
+public interface ITestService
 {
-    public interface ITestService
-    {
-        Task<bool> CreateTestAsync(CreateTestRequest request);
-        Task<object> GetTestListAsync(int? employeeId, int? courseId, string? testType);
-    }
+    Task<bool> CreateTestAsync(CreateTestRequest request, int recordedByEmpId);
+    Task<IEnumerable<TrainingTest>> GetTestListAsync(int? employeeId, int? courseId, string? testType);
 }
